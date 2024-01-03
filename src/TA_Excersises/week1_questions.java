0e6256201127260a67afd1fd8fb7f4ae4d982602
doronMor7 <154997625+doronMor7@users.noreply.github.com>
2024-01-03 21:46:46 +0200

    Update week1_questions.java
    I added 2 questions to PS1, hence I wrote the 2 additional answers.

--- src/TA_Excersises/week1_questions.java
+++ src/TA_Excersises/week1_questions.java
@@ -51,5 +51,18 @@ public class week1_questions {
         double farn = celsius*(9.0/5) + 32;
         System.out.println("The degrees in Fahrenheit is " + farn + "F");
 
+        //7
+        int bigSeconds = MyConsole.readInt("Enter the seconds num: ");
+        int hours = bigSeconds / 3600;
+        int minutes = (bigSeconds - (3600*hours) ) / 60;
+        int seconds = (bigSeconds - 3600*hours) ) % 60; 
+        System.out.println("The hours is "+hours+" hours, "+minutes+" minutes, and "+seconds+ "seconds");
+
+        //8
+        double radius = MyConsole.readDouble("Enter the radius: ");
+        double area = radius*radius*3.14159;
+        double Perimeter = radius * 2 * 3.14159;
+        System.out.println("The circle area is "+area+", and the perimeter is "+Perimeter);
+        
     }
 }
